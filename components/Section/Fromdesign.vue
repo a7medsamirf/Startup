779346d444c0792766a438d118212a954bd10e1c
ahmed samir <ahmed.samir.farid@gmail.com>
2022-10-08 18:41:50 +0200

    V 0.1

---
+++ components/Section/Fromdesign.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="Fromdesign pa-16">
+        
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Fromdesign",
+}
+</script>
+
+<style>
+
+</style>
