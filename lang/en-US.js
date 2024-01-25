a37c02461b8689186454bbb9f91d1070cedb2408
ahmed samir <ahmed.samir.farid@gmail.com>
2024-01-25 19:57:51 +0200

    9ada7f2

---
+++ lang/en-US.js
@@ -0,0 +1,68 @@
+export default () => {
+  return new Promise(function (resolve) {
+    resolve({
+      common: {
+        "discuss": "Let's discuss",
+        "Learn-more": "Learn More",
+        "reviewtitle1": "Super Helpful !!!!",
+        "reviewtitle2": "Awesome !!!!",
+        "reviewtitle3": "Great Support !!!!",
+        "comment": "There’s no other program that walks you through exactly what you need to know to start an online store fast, written by someone who has built several 7-figure ecommerce businesses from scratch. ",
+      },
+      nav: {
+        "pages": "Pages",
+        "Home": "Home",
+        "solutions": "Solutions",
+        "about-Us": "about Us ",
+        "Services": "Services",
+        "Feedback": "Feedback",
+        "dashboard": "User Dashboard",
+        "contacts": "contacts Us",
+        "faq": "Faq",
+      },
+      MobileBar: {
+        "home": "Home",
+        "Testimonials": "Testimonials",
+      },
+      HeroSection: {
+        "title": "The template your startup needed",
+        "subtitle": "A startup is a company or project undertaken by an entrepreneur to seek, develop, and validate a scalable business model.",
+      },
+      features: {
+        "section-title": "features",
+        "section-description": "Your Dream Template",
+        "Product": "Product Design",
+        "Branding": "Branding",
+        "Programming": "Programming",
+        "Marketing":"Online Marketing",
+        "description": "Startups typically begin by a founder (solo-founder) or co-founders who have a way to solve a problem. "
+      },
+      FromDesign:{
+        "title": "From design to code and back.",
+        "subtitle": " Startups typically begin by a founder (solo-founder) or co-founders who have a way to solve a problem. The founder of a startup will begin market validation by problem interview, solution interview, and building a minimum viable product (MVP), i.e. a prototype, to develop and validate their business models.",
+      },
+      Testimonials: {
+        "section-title": "Testimonials",
+        "section-description": "Trusted By 100'S Of Clients",
+        },
+      blog:{
+        "section-title": "Blog",
+        "section-description": "Knowledge Hub",
+        "title": "How A Website Growths your Business",
+        "description": "Startups typically begin by a founder (solo-founder) or co-founders who have a way to solve a problem.",
+      },
+      Newsletter:{
+        "title": "Fire up your Rocket!",
+        "description": "Startups typically begin by a founder (solo-founder) or co-founders who have a way to solve a problem.",
+      },
+      footer:{
+        "contactus": " Contact Us",
+        "Social-Media": "Social Media",
+        "QuickLinks": "Quick Links",
+        "Copyright": "Copyright",
+        "reserved": "All rights reserved.",
+      }
+    })
+  })
+}
+
